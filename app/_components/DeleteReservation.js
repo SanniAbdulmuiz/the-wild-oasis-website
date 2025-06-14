7b3e89ec3d27d9976ecb7e6b417f6d7693489cfd
Sanni Abdulmuiz <ayomideabdulmuiz565@gmail.com>
2025-06-14 02:37:58 +0100

    Deleting Bookings with the useOptimistic hook to improve UI performance

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,15 +1,14 @@
 "use client";
 import { TrashIcon } from "@heroicons/react/24/solid";
-import { deleteReservation } from "../_library/action";
 import { useTransition } from "react";
 import SpinnerMini from "./SpinnerMini";
 
-export default function DeleteReservation({ bookingId }) {
+export default function DeleteReservation({ bookingId, onDelete }) {
   const [isPending, startTransition] = useTransition(); //We're using this instead of the "usertatus" because we directly called a server action "deleteReservation" from a button not a form
 
   function handleDelete() {
     if (confirm("Are you sure you want to delete this reservation"))
-      startTransition(() => deleteReservation(bookingId));
+      startTransition(() => onDelete(bookingId));
   }
   return (
     <button
