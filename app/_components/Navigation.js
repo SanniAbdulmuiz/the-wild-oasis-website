0ca24a3817b8233bee009680e6790e8a7bd70a97
Sanni Abdulmuiz <ayomideabdulmuiz565@gmail.com>
2025-07-21 22:57:16 +0100

    Refactor Navigation component: improve code readability and structure

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -5,7 +5,7 @@ export default async function Navigation() {
   const session = await auth();
   return (
     <nav className="z-10 text-base sm:text-xl">
-      <ul className="flex gap-16 items-center">
+      <ul className="flex gap-8 sm:gap-16 items-center">
         <li>
           <Link
             href="/cabins"
