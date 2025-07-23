bc77ef5ec92a3642766956012f9ce53d12810c67
Sanni Abdulmuiz <ayomideabdulmuiz565@gmail.com>
2025-07-23 23:05:27 +0100

    Refactor Page component: enhance code structure and readability

--- app/page.js
+++ app/page.js
@@ -21,7 +21,7 @@ export default function Page() {
 
         <Link
           href="/cabins"
-          className="bg-accent-500 px-4 py-3 md:px-8 md:py-6 text-primary-800 text-base md:text-lg font-semibold hover:bg-accent-600 transition-all"
+          className="bg-accent-500 px-4 py-3 md:px-8 md:py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
         >
           Explore luxury cabins
         </Link>
