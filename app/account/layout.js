08d357cd69461fa4304a0aa874fdb2662831c8ff
Sanni Abdulmuiz <ayomideabdulmuiz565@gmail.com>
2025-07-30 15:57:28 +0100

    Refactor Layout component: enhance structure and improve readability

--- app/account/layout.js
+++ app/account/layout.js
@@ -2,7 +2,7 @@ import SideNavigation from "@/app/_components/SideNavigation";
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen sm:grid sm:grid-cols-[16rem_1fr] gap-12 flex">
+    <div className="min-h-screen sm:grid sm:grid-cols-[16rem_1fr] gap-4 flex">
       <SideNavigation />
 
       <main className="py-1 flex-1">{children}</main>
