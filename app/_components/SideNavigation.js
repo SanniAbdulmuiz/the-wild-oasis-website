65e4298f54bc35203e06aa44fe3020ac88ac8252
Sanni Abdulmuiz <ayomideabdulmuiz565@gmail.com>
2025-08-24 18:27:37 +0100

    Refactor SideNavigation component: improve code structure and readability

--- app/_components/SideNavigation.js
+++ app/_components/SideNavigation.js
@@ -81,7 +81,7 @@ function SideNavigation() {
               </li>
             ))}
 
-            <li className="mt-auto px-5 pb-5">
+            <li className="px-5 pb-5 sm:mt-auto">
               <SignOutButton />
             </li>
           </ul>
